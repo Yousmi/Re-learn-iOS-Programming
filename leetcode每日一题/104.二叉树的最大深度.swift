555df164e6985cb8ed1ec73b84777b4e8ebd3cda
Scan <q_zhixin@sina.com>
2021-06-22 00:06:33 +0800

    学习苹果文档

--- leetcode每日一题/104.二叉树的最大深度.swift
+++ leetcode每日一题/104.二叉树的最大深度.swift
@@ -10,6 +10,30 @@ import Foundation
 
 class Solution_104 {
     func maxDepth(_ root: TreeNode?) -> Int {
+        guard root != nil else {
+            return 0
+        }
+        var item = [TreeNode](), res = 0
+        item.append(root!)
+
+        while item.count > 0 {
+            res += 1
+            var inner = [TreeNode]()
+            for i in 0 ..< item.count {
+                let node = item[i]
+                if let left = node!.left {
+                    inner.append(left)
+                }
+                if let right = node!.right {
+                    inner.append(right)
+                }
+            }
+            item = inner
+        }
+        return res
+    }
+
+    func maxDepth_1(_ root: TreeNode?) -> Int {
         if root == nil {
             return 0
         }
