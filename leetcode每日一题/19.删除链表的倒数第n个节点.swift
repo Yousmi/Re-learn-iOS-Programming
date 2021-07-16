054bd254c87a31a009f299e6f91d952ddf519922
Scan <q_zhixin@sina.com>
2021-07-16 22:13:34 +0800

    修改 leetcode19 处理xmin总结

--- leetcode每日一题/19.删除链表的倒数第n个节点.swift
+++ leetcode每日一题/19.删除链表的倒数第n个节点.swift
@@ -1,5 +1,24 @@
 class Solution_19 {
+    //一些思考 
+    // 快节点需要先走到 n 步，然后 开始同步走，当快节点为尾节点时，快慢节点正好相差n 个，直接删除即可
+        
     func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        let dummy = ListNode(0)
+        dummy.next = head
+        var fast: ListNode? = dummy, slow: ListNode? = dummy
+        for _ in 0 ..< n {
+            fast = fast?.next
+        }
+        while fast?.next != nil {
+            fast = fast?.next
+            slow = slow?.next
+        }
+        slow?.next = slow?.next?.next
+
+        return dummy.next
+    }
+
+    func removeNthFromEnd_1(_ head: ListNode?, _ n: Int) -> ListNode? {
         let dummy = ListNode(0)
         dummy.next = head   
         
