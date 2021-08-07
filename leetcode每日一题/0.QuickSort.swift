cbc89557cd27acc6cffdb661b0b57140ffef521e
Scan <q_zhixin@sina.com>
2021-08-07 11:24:26 +0800

    buc

--- leetcode每日一题/0.QuickSort.swift
+++ leetcode每日一题/0.QuickSort.swift
@@ -21,7 +21,13 @@ class QuickSort {
         _quickSort(&nums, p, q - 1);
         _quickSort(&nums, q + 1, r);
     }
-    
+    //基准点的选择  
+    //选取一个锚点 然后递归排列左右2边进行排序
+    //1、选择最右边的节点的值 作为比较点  
+    //2、最左边的为起始点  
+    //3、从左往右循环遍历
+    //4、遇到比锚点小的，直接交换起始点和遍历下标的值 
+    //5、最终交换交换点跟锚点的值
     private func partition(_ nums: inout [Int], _ left: Int, _ right: Int) -> Int {
         let pivot = nums[right]
         var i = left
