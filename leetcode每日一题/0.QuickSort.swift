8c99651591bcf5c00e1edcc337e6512c27018251
Scan <q_zhixin@sina.com>
2021-08-08 10:48:08 +0800

    buc

--- leetcode每日一题/0.QuickSort.swift
+++ leetcode每日一题/0.QuickSort.swift
@@ -28,6 +28,7 @@ class QuickSort {
     //3、从左往右循环遍历
     //4、遇到比锚点小的，直接交换起始点和遍历下标的值 
     //5、最终交换交换点跟锚点的值
+    //逻辑点：选完基准点之后开始遍历然后开始比较，较小的就跟起始点开始交换
     private func partition(_ nums: inout [Int], _ left: Int, _ right: Int) -> Int {
         let pivot = nums[right]
         var i = left
@@ -41,5 +42,35 @@ class QuickSort {
             nums.swapAt(i, right)
         }
         return i
+    }
+}
+
+
+class quick {
+    func sort(_ arr: [Int]) {
+        var sor = arr
+
+        func r_sort(_ left: Int, _ right: Int) {
+            if right > left {
+                return
+            }
+            let p = partition(left, right)
+            r_sort(left, p - 1)
+            r_sort(p + 1, right)
+
+        }
+        func partition(_ left: Int, _ right: Int) -> Int {
+            var pv = sor[right], idx = left
+            for i in left ..< right {
+                if sor[i] < pv {
+                    sor.swapAt(idx, i)
+                    idx += 1
+                }
+            }
+            sor.swapAt(idx, right)
+            return idx
+        }
+
+        r_sort(0, sor.count - 1)
     }
 }
